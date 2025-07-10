77eeddfdf29841742d00ed39731747b63ae4b836
tranquangthong <23115053122141@sv.ute.udn.vn>
2025-07-10 15:43:51 +0700

    Xây dụng layout cho dự án

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const port = process.env.PORT;
 app.set("view engine", "pug");
 // Đường dẫn tới thư mục chứa các file pug (views)
 app.set("views", "./views");
+app.use(express.static("public"));
+// Cấu hình đường dẫn tới thư mục chứa các file tĩnh (CSS, JS,..)
 indexRoute(app);
 app.listen(port, () => {
   console.log(`Ví dụ đã chạy với cổng ${port}`);
