9ceef4f393424d22f1fda237f96f2ac280caab94
tranquangthong <23115053122141@sv.ute.udn.vn>
2025-07-10 11:08:03 +0700

    setup environment variables

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const indexRoute = require("./routes/client/index.route")
-const app = express()
-const port = 3000
+const express = require("express");
+require("dotenv").config();
+const indexRoute = require("./routes/client/index.route");
+const app = express();
+const port = process.env.PORT;
 // Cấu hình view engine là pug
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 // Đường dẫn tới thư mục chứa các file pug (views)
-app.set('views', './views');
-indexRoute(app)
+app.set("views", "./views");
+indexRoute(app);
 app.listen(port, () => {
-  console.log(`Ví dụ đã chạy với cổng ${port}`)
-})
+  console.log(`Ví dụ đã chạy với cổng ${port}`);
+});
